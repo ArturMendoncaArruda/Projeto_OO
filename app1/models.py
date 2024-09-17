9c8c57d2b5c5425f16549c3b800200af762173a0
Artur <arturmendoncaarruda@gmail.com>
2024-09-17 20:17:45 -0300

    Beta version

--- app1/models.py
+++ app1/models.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Dados_jogo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Relaciona com o auth_user
-    usuario_nome = models.CharField(max_length=300)
-    usuario_senha = models.CharField(max_length=70)
+# Classe base abstrata
+class BaseJogo(models.Model):
     dinheiro = models.IntegerField(default=0)
     tempo = models.IntegerField(default=0)
     inimigos_derrotados = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True  # Define esta classe como abstrata, sem criar uma tabela no banco de dados
+
+# Classe que herda da base
+class Dados_jogo(BaseJogo):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Relaciona com o auth_user
+    usuario_nome = models.CharField(max_length=300)
+    usuario_senha = models.CharField(max_length=70)
